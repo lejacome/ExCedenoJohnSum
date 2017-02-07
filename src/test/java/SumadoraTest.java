0a5455920f89ede83bea13a4639d242b36091f48
John <jjjohncedeno@gmail.com>
2017-02-07 11:10:14 -0500

    Pruebas adicionales #1. Se ejecutan nuevas pruebas para verificar que la versión es la correcta.

--- src/test/java/SumadoraTest.java
+++ src/test/java/SumadoraTest.java
@@ -17,5 +17,23 @@ public class SumadoraTest {
 		assertEquals(5, s.sumar());
 		
 	}
+	
+	@Test
+	public void cuandoUnSoloNumeroEsUsadoSeRetornaEseValor() {
+		int[] a = {3};
+		when(c.convertir("3")).thenReturn(a);
+		
+		Sumadora s = new Sumadora(c.convertir("3"));
+		assertEquals(3, s.sumar());
+	}
+
+	@Test
+	public void cuandoDosNumerosSonUsadosEntoncesRetornamosSuSuma() {
+		int[] a = {3,6};
+		when(c.convertir("3,6")).thenReturn(a);
+		
+		Sumadora s = new Sumadora(c.convertir("3,6"));
+		assertEquals(9, s.sumar());
+	}
 
 }
